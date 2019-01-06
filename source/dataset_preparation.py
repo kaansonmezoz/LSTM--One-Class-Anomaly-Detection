255b775767ef427d85f60f733994364b7ccd23df
unknown <ksonmezoz@outlook.com>
2019-01-06 18:11:28 +0300

    Normalization function has been added

--- source/dataset_preparation.py
+++ source/dataset_preparation.py
@@ -18,3 +18,45 @@ def get_datasets(train_set_ADL, window_size, random_state_test, random_state_val
     train_set_ADL = reshape_data_frame_for_LSTM(train_set_ADL, window_size)
     
     return split_datasets(train_set_ADL, random_state_test, random_state_validation)
+
+def normalization(dataset): ### acc-x acc-y acc-z ori-x ori-y ori-z gyro-x gyro-y gyro-z
+    import pandas as pd
+    
+    sensor_range_max = {'acc-x': [], 'acc-y': [], 'acc-z': [], 'ori-x': [], 'ori-y': [], 'ori-z': [], 'gyro-x': [], 'gyro-y': [], 'gyro-z': []}
+    sensor_range_min = {'acc-x': [], 'acc-y': [], 'acc-z': [], 'ori-x': [], 'ori-y': [], 'ori-z': [], 'gyro-x': [], 'gyro-y': [], 'gyro-z': []}
+    
+    
+    for i in range(len(dataset['acc-x'])):
+    
+        sensor_range_max['acc-x'].append(dataset.max()['acc-x'])  ### Mobifall_2.0 datasetinde olcumde kullanilan sensorlerin olcebilecegi araliklar  baz alinarak bu degerler belirlendi
+        sensor_range_max['acc-y'].append(dataset.max()['acc-y'])
+        sensor_range_max['acc-z'].append(dataset.max()['acc-z'])
+
+        sensor_range_min['acc-x'].append(dataset.min()['acc-x'])
+        sensor_range_min['acc-y'].append(dataset.min()['acc-y'])
+        sensor_range_min['acc-z'].append(dataset.min()['acc-z'])
+
+        sensor_range_max['ori-x'].append(dataset.max()['ori-x'])
+        sensor_range_max['ori-y'].append(dataset.max()['ori-y'])
+        sensor_range_max['ori-z'].append(dataset.max()['ori-z'])
+
+        sensor_range_min['ori-x'].append(dataset.min()['ori-x'])
+        sensor_range_min['ori-y'].append(dataset.min()['ori-y'])
+        sensor_range_min['ori-z'].append(dataset.min()['ori-z'])
+
+        sensor_range_max['gyro-x'].append(dataset.max()['gyro-x'])
+        sensor_range_max['gyro-y'].append(dataset.max()['gyro-y'])
+        sensor_range_max['gyro-z'].append(dataset.max()['gyro-z'])
+
+        sensor_range_min['gyro-x'].append(dataset.min()['gyro-x'])
+        sensor_range_min['gyro-y'].append(dataset.min()['gyro-y'])
+        sensor_range_min['gyro-z'].append(dataset.min()['gyro-z'])
+
+    df_max = pd.DataFrame.from_dict(sensor_range_max)
+    df_min = pd.DataFrame.from_dict(sensor_range_min)
+    
+    dataset = (2 * (dataset - df_min) / (df_max - df_min)) - 1
+
+    print(dataset)
+
+    return dataset
