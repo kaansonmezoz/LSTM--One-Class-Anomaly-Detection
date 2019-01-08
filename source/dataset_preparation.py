3467770694d2ac318e9425ac3163b36ab091e871
unknown <ksonmezoz@outlook.com>
2019-01-08 18:55:01 +0300

    normalization() has been refactored also max and min values are now constant

--- source/dataset_preparation.py
+++ source/dataset_preparation.py
@@ -1,9 +1,9 @@
 from sklearn.model_selection import train_test_split
 
 def split_datasets(train_set, random_state_test, random_state_validation):
-    ### Splitting dataset into train set, test set, validation set
-    train_set_ADL, test_set_ADL = train_test_split(train_set, test_size = 0.20, random_state = random_state_test)  ### önce %80 train, %20 test
-    train_set_ADL, validation_set_ADL = train_test_split(train_set_ADL, test_size = 0.25, random_state = random_state_validation) ### %80'in %25'i totaldeki verinin %20sine denk geliyor
+    ### Splitting dataset into train set, test set, validation set (60%, 20%, 20%)
+    train_set_ADL, test_set_ADL = train_test_split(train_set, test_size = 0.20, random_state = random_state_test)  ### 80% train, 20% test
+    train_set_ADL, validation_set_ADL = train_test_split(train_set_ADL, test_size = 0.25, random_state = random_state_validation) ### 25% 0f 80% equals the 20% of the total
     
     return train_set_ADL, validation_set_ADL, test_set_ADL
 
@@ -19,44 +19,39 @@ def get_datasets(train_set_ADL, window_size, random_state_test, random_state_val
     
     return split_datasets(train_set_ADL, random_state_test, random_state_validation)
 
-def normalization(dataset): ### acc-x acc-y acc-z ori-x ori-y ori-z gyro-x gyro-y gyro-z
-    import pandas as pd
+def normalization(dataset): ### acc-x acc-y acc-z ori-x ori-y ori-z gyro-x gyro-y gyro-z    
+    g = 9.81
+
+    ### based on the sensors used to obtain samples (reference: Analysis of Public Datasets for Wearable Fall Detection Systems)
+    sensor_max_value = {
+        'acc-x': 1.999 * g, 
+        'acc-y': 1.999 * g, 
+        'acc-z': 1.999 * g, 
+        'ori-x': 360, 
+        'ori-y': 360, 
+        'ori-z': 360, 
+        'gyro-x': 10.007805, ###573.42, Normalde max deger bu sensorun olcebilecegi ama textlerdeki max deger 10.007805 olmus bu kullanildiki elde edilen olcek degeri cok kucuk olmasin
+        'gyro-y': 10.007805, ###573.42, 
+        'gyro-z': 10.007805, ###573.42
+    }  
     
-    sensor_range_max = {'acc-x': [], 'acc-y': [], 'acc-z': [], 'ori-x': [], 'ori-y': [], 'ori-z': [], 'gyro-x': [], 'gyro-y': [], 'gyro-z': []}
-    sensor_range_min = {'acc-x': [], 'acc-y': [], 'acc-z': [], 'ori-x': [], 'ori-y': [], 'ori-z': [], 'gyro-x': [], 'gyro-y': [], 'gyro-z': []}
+    sensor_min_value = {
+        'acc-x': -1.9951 * g,
+        'acc-y': -1.9951 * g, 
+        'acc-z': -1.9951 * g, 
+        'ori-x': -179.9995,     
+        'ori-y': -179.9995,
+        'ori-z': -179.9995, 
+        'gyro-x': -10.007500,  #-573.44, normalde min deger bu sensorun olcebilecegi ama textlerde kullanilan min deger -10.007500 olmus diger turlu -573.44'u kullaninca fazla kucuk oluyor
+        'gyro-y': -10.007500,  #-573.44, 
+        'gyro-z': -10.007500,  #-573.44 
+    }
     
-    
-    for i in range(len(dataset['acc-x'])):
-    
-        sensor_range_max['acc-x'].append(dataset.max()['acc-x'])  ### Mobifall_2.0 datasetinde olcumde kullanilan sensorlerin olcebilecegi araliklar  baz alinarak bu degerler belirlendi
-        sensor_range_max['acc-y'].append(dataset.max()['acc-y'])
-        sensor_range_max['acc-z'].append(dataset.max()['acc-z'])
-
-        sensor_range_min['acc-x'].append(dataset.min()['acc-x'])
-        sensor_range_min['acc-y'].append(dataset.min()['acc-y'])
-        sensor_range_min['acc-z'].append(dataset.min()['acc-z'])
-
-        sensor_range_max['ori-x'].append(dataset.max()['ori-x'])
-        sensor_range_max['ori-y'].append(dataset.max()['ori-y'])
-        sensor_range_max['ori-z'].append(dataset.max()['ori-z'])
-
-        sensor_range_min['ori-x'].append(dataset.min()['ori-x'])
-        sensor_range_min['ori-y'].append(dataset.min()['ori-y'])
-        sensor_range_min['ori-z'].append(dataset.min()['ori-z'])
-
-        sensor_range_max['gyro-x'].append(dataset.max()['gyro-x'])
-        sensor_range_max['gyro-y'].append(dataset.max()['gyro-y'])
-        sensor_range_max['gyro-z'].append(dataset.max()['gyro-z'])
-
-        sensor_range_min['gyro-x'].append(dataset.min()['gyro-x'])
-        sensor_range_min['gyro-y'].append(dataset.min()['gyro-y'])
-        sensor_range_min['gyro-z'].append(dataset.min()['gyro-z'])
-
-    df_max = pd.DataFrame.from_dict(sensor_range_max)
-    df_min = pd.DataFrame.from_dict(sensor_range_min)
-    
-    dataset = (2 * (dataset - df_min) / (df_max - df_min)) - 1
-
-    print(dataset)
+    for col_name in dataset.columns:
+        max_value = sensor_max_value[col_name] ### bunun yerine daha fix bir scale edilebilmesi adina sensorlerin araliklarini koyacaktik hatirlarsan
+        min_value = sensor_min_value[col_name]
+        
+        ### Normalizing dataset into (-1, 1) 
+        dataset[col_name] = (2 * (dataset[col_name] - min_value) / (max_value - min_value)) - 1    
 
     return dataset
