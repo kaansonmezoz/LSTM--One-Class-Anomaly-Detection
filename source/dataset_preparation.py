5f1a128550037580847ebd2b5d0ef2281be9ef45
unknown <ksonmezoz@outlook.com>
2019-01-09 17:25:22 +0300

    Normalizing the datasets

--- source/dataset_preparation.py
+++ source/dataset_preparation.py
@@ -18,3 +18,40 @@ def get_datasets(train_set_ADL, window_size, random_state_test, random_state_val
     train_set_ADL = reshape_data_frame_for_LSTM(train_set_ADL, window_size)
     
     return split_datasets(train_set_ADL, random_state_test, random_state_validation)
+
+def normalization(dataset): ### acc-x acc-y acc-z ori-x ori-y ori-z gyro-x gyro-y gyro-z    
+    g = 9.81
+
+    ### based on the sensors used to obtain samples (reference: Analysis of Public Datasets for Wearable Fall Detection Systems)
+    sensor_max_value = {
+        'acc-x': 1.999 * g, 
+        'acc-y': 1.999 * g, 
+        'acc-z': 1.999 * g, 
+        'ori-x': 360, 
+        'ori-y': 360, 
+        'ori-z': 360, 
+        'gyro-x': 573.42, 
+        'gyro-y': 573.42, 
+        'gyro-z': 573.42
+    }  
+    
+    sensor_min_value = {
+        'acc-x': -1.9951 * g,
+        'acc-y': -1.9951 * g, 
+        'acc-z': -1.9951 * g, 
+        'ori-x': -179.9995,     
+        'ori-y': -179.9995,
+        'ori-z': -179.9995, 
+        'gyro-x': -573.44, 
+        'gyro-y': -573.44, 
+        'gyro-z': -573.44 
+    }
+    
+    for col_name in dataset.columns:
+        max_value = sensor_max_value[col_name] ### bunun yerine daha fix bir scale edilebilmesi adina sensorlerin araliklarini koyacaktik hatirlarsan
+        min_value = sensor_min_value[col_name]
+        
+        ### Normalizing dataset into (-1, 1) 
+        dataset[col_name] = (2 * (dataset[col_name] - min_value) / (max_value - min_value)) - 1    
+
+    return dataset
